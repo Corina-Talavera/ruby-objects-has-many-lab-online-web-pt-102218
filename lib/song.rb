2e24f0e5a6bd97083f59af8aa3b82ef074c5f8e3
Corina Talavera <corina.talavera@gmail.com>
2018-11-18 18:55:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,17 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :artist, :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
-    end
+    self.artist != nil ? self.artist.name : nil
   end
 end
